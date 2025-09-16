b4261f5049f880fa3ab22533eb8671e696d06e4e
Jon Jozwiak <jonjozwiak@users.noreply.github.com>
2025-09-16 14:20:13 +0000

    Update package.json

--- hello-js-action/index.js
+++ hello-js-action/index.js
@@ -1,13 +1,18 @@
-import core from "@actions/core";
-import github from "@actions/github";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-async function run() {
-  try {
-    const name = core.getInput("name");
-    core.info(`Hello ${name}!`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput("who-to-greet");
+  core.info(`Hello ${nameToGreet}!`);
+
+  // Get the current time and set it as an output variable
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  core.info(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-run();
